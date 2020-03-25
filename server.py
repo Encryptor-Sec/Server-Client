1bfa1a4b3ba30d42c918a8c49af4b3d25c69cd4c
Encryptor <62632966+Encryptor-Sec@users.noreply.github.com>
2020-03-25 19:20:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -10,16 +10,17 @@ logo= '''\033[1;33
 print(logo) 
 import socket
 def server():
+    print('Waiting for client...')
     s = socket.socket()
     s.bind((socket.gethostname(),5000))
     s.listen(1)
     conn,address = s.accept()
-    print("Connected To :- " + str(address))
+    print("Connected To Client :- IP ->" + str(address))
     while True:
         data = conn.recv(1024).decode()
         if not data:
             break
-        print("From client : " + str(data))
+        print("From Client : " + str(data))
         data = input('Your Message : ')
         conn.send(data.encode())
     conn.close()
